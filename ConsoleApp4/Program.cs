d1e5a6e4a6497d6592af4bfe6f17a0865c37d51e
fidanaliizade <bd7h0ble7@code.edu.az>
2023-09-29 12:03:16 +0400

    Proje dosyası ekle.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,69 @@
+﻿//==========================task1===================
+
+//for (int i = 0; i <=25; i++)
+//{
+//    if (i * i >=1 && i*i<=25)
+//        {
+//            Console.WriteLine("Numbers: "+i);
+//        }
+
+//    }
+
+
+
+
+
+
+
+
+
+//================task2=================
+
+
+//int[] numbers = { 2, 77, 71, 93, 775 };
+//int sum = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] %2==1)
+//        sum += numbers[i];
+
+//}
+
+//Console.WriteLine(sum);
+
+
+
+//==================task3==================
+//int[] numbers = { 1, 77, 71, 93, 775 };
+//int sum = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (i % 2 != 0)
+//    {
+//        sum += numbers[i];
+//    }
+//}
+//Console.WriteLine(sum);
+
+
+//=====================task4========================
+//Console.WriteLine("Enter string"); 
+//string text = Console.ReadLine();
+//bool your_question = true;
+
+//for (int i = 0; i*i < text.Length; i++)
+//{
+//    if (text[i] != text[ text.Length-1-i])
+//    { your_question = false; 
+//        break;
+//    }
+//}
+//if ( your_question )
+//{
+//    Console.WriteLine("This word is  polindrome");
+//}
+//else
+//{
+//    Console.WriteLine("This word is not polindrome");
+//}
+
